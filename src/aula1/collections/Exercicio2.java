2c7450efcb65bc77e41a58a48fa910a519dde3a0
rodnei-c <rmcaetano04@gmail.com>
2024-07-29 15:20:12 -0300

    exercicio 2

--- src/aula1/collections/Exercicio2.java
+++ src/aula1/collections/Exercicio2.java
@@ -5,18 +5,20 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class Exercicio1 {
+public class Exercicio2 {
 
 	public static void main(String[] args) {
+
 		List<Integer> lista = new ArrayList<>();
-		
-		while(lista.size() < 50) {
-		
-		lista.add(new Random().nextInt(60));
-		Collections.sort(lista);
+
+		while (lista.size() < 50) {
+
+			lista.add(new Random().nextInt(60));
+			Collections.sort(lista.reversed());
+			
 		}
-		System.out.print(lista);		
-		
+		System.out.println(lista);
+
 	}
-	
+
 }
