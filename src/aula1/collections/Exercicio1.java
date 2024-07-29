3f4bfc5e0640aa1fbca95acc149f7bfca0ee4de2
rodnei-c <rmcaetano04@gmail.com>
2024-07-29 15:09:57 -0300

    exercicio 1

--- src/aula1/collections/Exercicio1.java
+++ src/aula1/collections/Exercicio1.java
@@ -1,5 +1,23 @@
 package aula1.collections;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Exercicio1 {
 
+	public static void main(String[] args) {
+		List<Integer> lista = new ArrayList<>();
+		
+		while(lista.size() < 50) {
+		
+		lista.add(new Random().nextInt());
+		Collections.sort(lista);
+		}
+		System.out.print(lista);		
+		
+	}
+	
 }
