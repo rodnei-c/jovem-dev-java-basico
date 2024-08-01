a907a98d8cb1eb24790e2d77930c8cec1fed66de
rodnei-c <rmcaetano04@gmail.com>
2024-08-01 15:50:25 -0300

    aula 4 exercicio 2

---
+++ src/aula4/lamba/Aluno.java
@@ -0,0 +1,33 @@
+package aula4.lamba;
+
+import java.time.LocalDate;
+
+public class Aluno {
+	private String nome;
+	private int numeroChamada;
+	private LocalDate dataNascimento;
+	
+	public Aluno(String nome, int numeroChamada, LocalDate dataNascimento) {
+		this.nome = nome;
+		this.numeroChamada = numeroChamada;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getNumeroChamada() {
+		return numeroChamada;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	
+	public String dados() {
+		return "\nNome: " + this.getNome() + "\nNumero de chamada: " + this.getNumeroChamada() + "\nData de Nascimento: " + this.getDataNascimento();
+	}
+	
+	
+}
---
+++ src/aula4/lamba/Exercicio2.java
@@ -0,0 +1,32 @@
+package aula4.lamba;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		
+		List<Aluno> lista = new ArrayList<Aluno>();
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		for(int i = 0; i < 5; i++) {
+			lista.add(new Aluno(JOptionPane.showInputDialog("Informe o nome: "),
+					Integer.parseInt(JOptionPane.showInputDialog("Informe o numero de chamada: ")), 
+					LocalDate.parse(JOptionPane.showInputDialog("Informe a data de nascimento: "), df)));
+		}
+		
+		lista.sort(Comparator.comparing(Aluno::getDataNascimento));
+		
+		for (Aluno aluno : lista) {
+			System.out.println(aluno.dados());
+		}
+		
+	}
+	
+}
