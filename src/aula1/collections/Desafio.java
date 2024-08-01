36aa668ae5542f110e3f26ca5bb97d96feea3405
rodnei-c <rmcaetano04@gmail.com>
2024-08-01 19:12:11 -0300

    desafio aula 1

--- src/aula1/collections/Desafio.java
+++ src/aula1/collections/Desafio.java
@@ -1,7 +1,63 @@
 package aula1.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
 public class Desafio {
 
-	
-	
+	public static void main(String[] args) {
+
+		Map<Character, List<String>> mapa = new HashMap<>();
+
+		for (char c : new char[] { 'a', 'e', 'i', 'o', 'u' }) {
+			mapa.put(c, new ArrayList<>());
+		}
+
+		while (true) {
+			String palavra = JOptionPane.showInputDialog("Informe uma palavra: ");
+
+			if (palavra.equalsIgnoreCase("fim")) {
+				break;
+			}
+
+			if (palavra.contains("a")) {
+				mapa.get('a').add(palavra);
+			}
+
+			if (palavra.contains("e")) {
+				mapa.get('e').add(palavra);
+			}
+
+			if (palavra.contains("i")) {
+				mapa.get('i').add(palavra);
+			}
+
+			if (palavra.contains("o")) {
+				mapa.get('o').add(palavra);
+			}
+
+			if (palavra.contains("u")) {
+				mapa.get('u').add(palavra);
+			}
+
+		}
+		
+		Collections.sort(mapa.get('a'));
+		Collections.sort(mapa.get('e'));
+		Collections.sort(mapa.get('i'));
+		Collections.sort(mapa.get('o'));
+		Collections.sort(mapa.get('u'));
+		
+		for(Map.Entry<Character, List<String>> entry: mapa.entrySet()) {		
+			
+			System.out.println(entry.getKey() + " - " + entry.getValue());
+			
+		}
+
+	}
 }
